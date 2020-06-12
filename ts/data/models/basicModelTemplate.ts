611aa7ab2570b8f7569630b80353c161a3f80e49
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-06-12 17:32:54 -0400

    adding routers and models

--- ts/data/models/basicModelTemplate.ts
+++ ts/data/models/basicModelTemplate.ts
@@ -1,5 +1,36 @@
-import db from '../dbConfig'; 
+import db from "../dbConfig";
 
-interface ModelTemplate<T> {
-    tableName: string
+interface ModelTemplateArg<T> {
+  tableName: string;
+  keyColumnName?: string;
+  preprocessData?: (data: T) => any;
+  processResult?: (result) => T;
 }
+
+export const basicModelTemplate = <T>({
+  tableName,
+  keyColumnName = "id",
+  preprocessData = (data: T) => data,
+  processResult = (result) => result as T,
+}: ModelTemplateArg<T>) => {
+  const get = (where: T = {} as T) =>
+    db(tableName)
+      .where(where)
+      .then((data) =>
+        data !== undefined ? data.map(processResult) : undefined
+      );
+
+  interface InsertArg {
+    item: T;
+  }
+
+  const insert = ({ item }: InsertArg) =>
+    db(tableName)
+      .insert(preprocessData(item), keyColumnName)
+      .then(([id]) => get({ [keyColumnName]: id } as T));
+
+  interface UpdateArg {
+      keyValue: any,
+      changes: T,
+  }    
+};

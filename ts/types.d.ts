f8b7fa1d260c333d54d757a4cc086c0d7d6f882a
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-06-16 19:19:40 -0400

    added UserDetails interface

--- ts/types.d.ts
+++ ts/types.d.ts
@@ -3,3 +3,39 @@ export interface UserCreds {
     username: string;
     password?: string;
   }
+
+  export type Role = 'admin' | 'user'
+
+export interface UserDetails {
+  id?: number,
+  user_id?: number,
+  education?: string;
+  major?: string;
+  avgmajor?: number;
+  lowmajor?: number;
+  highmajor?: number;
+  city?: string;
+  colindex?: number;
+  rent?: number;
+  avgwage?: number;
+  rentindex?: number;
+  colplusrentindex?: number;
+  groceriesindex?: number;
+  restaurantpriceindex?: number;
+  utilities?: number;
+  groceries?: number;
+  restaurant?: number;
+  premiums?: number;
+  medExpenses?: number;
+  carPayment?: number;
+  insurance?: number;
+  gas?: number;
+  carMaintenance?: number;
+  internet?: number;
+  cell?: number;
+  tv?: number;
+  studentLoans?: number;
+  clothing?: number;
+  entertainment?: number;
+  pOther?: number;
+}

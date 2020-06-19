f6aba74e41fda0f7ce49f2ebfa97f6641859fc41
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-06-18 21:32:39 -0400

    adding fields to user_details

--- ts/data/migrations/20200617222401_user_details.ts
+++ ts/data/migrations/20200617222401_user_details.ts
@@ -4,6 +4,15 @@ const makeUserDetailsTable = async (knex: Knex) => {
   return knex.schema.createTable("user_details", (tbl) => {
     tbl.increments();
 
+    tbl
+    .integer("user_id")
+    .unsigned()
+    .notNullable()
+    .references("id")
+    .inTable("user_creds")
+    .onUpdate("CASCADE")
+    .onDelete("CASCADE");
+
     tbl.string("education", 255);
 
     tbl.string("major", 255);
@@ -16,14 +25,52 @@ const makeUserDetailsTable = async (knex: Knex) => {
 
     tbl.string("city", 255);
 
-    tbl
-    .integer("user_id")
-    .unsigned()
-    .notNullable()
-    .references("id")
-    .inTable("user_creds")
-    .onUpdate("CASCADE")
-    .onDelete("CASCADE");
+    tbl.float("colindex");
+
+    tbl.float("rent");
+    
+    tbl.float("avgwage");
+
+    tbl.float("rentindex");
+
+    tbl.float("colplusrentindex");
+
+    tbl.float("groceriesindex");
+
+    tbl.float("restaurantpriceindex");
+
+    tbl.float("utilities");
+
+    tbl.float("groceries");
+
+    tbl.float("restaurant");
+    
+    tbl.float("premiums");
+
+    tbl.float("medExpenses");
+
+    tbl.float("carPayment");
+
+    tbl.float("insurance");
+
+    tbl.float("gas");
+
+    tbl.float("carMaintenance");
+
+    tbl.float("internet");
+
+    tbl.float("cell");
+
+    tbl.float("tv");
+
+    tbl.float("studentLoans");
+
+    tbl.float("clothing");
+
+    tbl.float("entertainment");
+
+    tbl.float("pOther");
+    
   });
 };
 

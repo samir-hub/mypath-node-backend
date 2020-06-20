890c6cc707b6005d29db7a69e7b8455e716dfbab
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-06-20 13:26:28 -0400

    added type checking to user_details model

---
+++ ts/data/models/user_details.ts
@@ -0,0 +1,15 @@
+import db from "../dbConfig";
+import { UserDetails } from "../../types";
+import { basicModelTemplate } from "./basicModelTemplate";
+
+const tableName = "user_creds";
+
+const { get, insert } = basicModelTemplate<UserDetails>({
+  db,
+  tableName,
+});
+
+export default {
+  get,
+  insert,
+};
